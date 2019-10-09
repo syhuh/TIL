a6d1970155bfe255148c4302d9dec7f9e851c35e
syhuh <syhuh1125@gmail.com>
2019-10-09 23:04:58 +0900

    Add 01_Intro & Getting Started/01_SectionIntro_File_Setup and 02_Using_the_Console

---
+++ documents/JavaScript/Udemy - Modern JavaScript From The Beginning/01_Intro & Getting Started/02_Using_the_Console/app.js
@@ -0,0 +1,22 @@
+console.log('Hello World');
+console.log(123);
+console.log(true);
+
+var greeting = "Hello";
+console.log(greeting);
+console.log({a:1, b:2});
+
+console.table({a:1, b:2});
+
+console.error('This is some error');
+
+console.clear();
+console.warn('This is a warining');
+
+console.time('Hello');
+  console.log('Hello World');
+  console.log('Hello World');
+  console.log('Hello World');
+  console.log('Hello World');
+  console.log('Hello World');
+console.timeEnd('Hello');
