46d4fbf857189b32e2310fa7ee30dccf5b80b557
syhuh <syhuh1125@gmail.com>
2019-10-10 11:30:34 +0900

    Add 01_Intro & Getting Started/04_Data_Types_in_JavaScript, 05_Type_Conversion, 06_Numbers_the_Math_Object

---
+++ documents/JavaScript/Udemy - Modern JavaScript From The Beginning/01_Intro & Getting Started/04_Data_Types_in_JavaScript/app.js
@@ -0,0 +1,35 @@
+// PRIMITIVE
+
+// String
+const name = 'John Doe';
+console.log(typeof name);
+// Number
+const age = 45;
+console.log(typeof age);
+// Boolean
+const hasKids = true;
+console.log(typeof hasKids);
+// Null
+const car = null;
+console.log(typeof car);
+// Undefined
+let test;
+console.log(typeof test);
+// Symbol
+const sym = Symbol();
+console.log(typeof sym);
+
+// REFERENCE TYPES - Objects
+// Array
+const hobbies = ['movies', 'music'];
+console.log(typeof hobbies);
+// Object literal
+const address = {
+  city: 'Boston',
+  state: 'MA'
+}
+console.log(typeof address);
+
+const today = new Date();
+console.log(today);
+console.log(typeof today);
---
+++ documents/JavaScript/Udemy - Modern JavaScript From The Beginning/01_Intro & Getting Started/05_Type_Conversion/app.js
@@ -0,0 +1,87 @@
+let val;
+
+// // Number to string
+// val = String(5);
+// val = String(4+4);
+// console.log(val);
+// console.log(typeof val);
+// console.log(val.length);
+// // Bool to string
+// val = String(true);
+// console.log(val);
+// console.log(typeof val);
+// console.log(val.length);
+// // Date to string
+// val = String(new Date());
+// console.log(val);
+// console.log(typeof val);
+// console.log(val.length);
+// // Array to string
+// val = String([1, 2, 3, 4]);
+// console.log(val);
+// console.log(typeof val);
+// console.log(val.length);
+
+// // toString()
+// val = (5).toString();
+// console.log(val);
+// console.log(typeof val);
+// console.log(val.length);
+// val = (true).toString();
+// console.log(val);
+// console.log(typeof val);
+// console.log(val.length);
+
+// // String to number
+// val = Number('5');
+// console.log(val);
+// console.log(typeof val);
+// console.log(val.toFixed());
+
+// // Bool to number
+// val = Number(true);
+// console.log(val);
+// console.log(typeof val);
+// console.log(val.toFixed());
+// val = Number(false);
+// console.log(val);
+// console.log(typeof val);
+// console.log(val.toFixed());
+
+// // Null to number
+// val = Number(null);
+// console.log(val);
+// console.log(typeof val);
+// console.log(val.toFixed());
+
+// // String to number (NaN)
+// val = Number('hello');
+// console.log(val);
+// console.log(typeof val);
+// console.log(val.toFixed());
+
+// // Array to number (NaN)
+// val = Number([1,2,3]);
+// console.log(val);
+// console.log(typeof val);
+// console.log(val.toFixed());
+
+// // parseInt
+// val = parseInt('100.80');
+// console.log(val);
+// console.log(typeof val);
+// console.log(val.toFixed());
+
+// // parseFloat
+// val = parseFloat('100.80');
+// console.log(val);
+// console.log(typeof val);
+// console.log(val.toFixed(3));
+
+// Type Coercion
+const val1 = String(5);
+const val2 = 6;
+const sum = val1 + val2;
+
+console.log(sum);
+console.log(typeof sum);
