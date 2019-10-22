15f7cd51b1e6109e8328912a35b4a7175e3fb9b6
syhuh <syhuh1125@gmail.com>
2019-10-22 09:32:35 +0900

    Add 06_Asynchronous JavaScript, Ajax & Fetch API/11_The_Fetch_API

---
+++ documents/JavaScript/Udemy - Modern JavaScript From The Beginning/06_Asynchronous JavaScript, Ajax & Fetch API/11_The_Fetch_API/app.js
@@ -0,0 +1,58 @@
+document.getElementById('button1').addEventListener('click', getText);
+document.getElementById('button2').addEventListener('click', getJson);
+document.getElementById('button3').addEventListener('click', getExternal);
+
+// Get local text file data
+function getText() {
+  fetch('test.txt')
+    .then(function(res) {
+      return res.text();
+    })
+    .then(function(data) {
+      console.log(data);
+      document.getElementById('output').innerHTML = data;
+    })
+    .catch(function(err) {
+      console.log(err);
+    });
+}
+
+// Get local json file data
+function getJson() {
+  fetch('posts.json')
+    .then(function(res) {
+      return res.json();
+    })
+    .then(function(data) {
+      console.log(data);
+
+      let out = '';
+      data.forEach(function(post) {
+        out += `<li>${post.title}</li>`;
+      });
+      document.getElementById('output').innerHTML = out;
+    })
+    .catch(function(err) {
+      console.log(err);
+    });
+}
+
+// Get from external API
+function getExternal() {
+  fetch('https://api.github.com/users')
+    .then(function(res) {
+      return res.json();
+    })
+    .then(function(data) {
+      console.log(data);
+
+      let out = '';
+      data.forEach(function(user) {
+        out += `<li>${user.login}</li>`;
+      });
+      document.getElementById('output').innerHTML = out;
+    })
+    .catch(function(err) {
+      console.log(err);
+    });
+}
