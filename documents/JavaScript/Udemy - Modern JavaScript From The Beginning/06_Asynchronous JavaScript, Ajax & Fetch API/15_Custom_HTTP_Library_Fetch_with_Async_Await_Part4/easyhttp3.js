8a4e02b1f5c98cdad709d815312acc55cf758bfa
syhuh <syhuh1125@gmail.com>
2019-10-25 15:30:00 +0900

    Add 06_Asynchronous JavaScript, Ajax & Fetch API/15_Custom_HTTP_Library_Fetch_with_Async_Await_Part4

---
+++ documents/JavaScript/Udemy - Modern JavaScript From The Beginning/06_Asynchronous JavaScript, Ajax & Fetch API/15_Custom_HTTP_Library_Fetch_with_Async_Await_Part4/easyhttp3.js
@@ -0,0 +1,56 @@
+/**
+ * EasyHTTP Library
+ * Library for making HTTP request
+ * 
+ * @version 3.0.0
+ * @author SYHuh
+ * @license MIT
+ * 
+ **/
+
+class EasyHTTP {
+  // Make an HTTP GET Request
+  async get(url) {
+    const res = await fetch(url);
+    const data = await res.json();
+    return data;
+  }
+
+  // Make an HTTP Post Request
+  async post(url, bodyData) {
+    const res = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify(bodyData)
+    });
+    const data = await res.json();
+    return data;
+  }
+
+  // Make an HTTP PUT Request
+  async put(url, bodyData) {
+    const res = await fetch(url, {
+      method: 'PUT',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify(bodyData)
+    });
+    const data = await res.json();
+    return data;
+  }
+
+  // Make an HTTP DELETE Request
+  async delete(url) {
+    const res = await fetch(url, {
+      method: 'DELETE',
+      headers: {
+        'Content-type': 'application/json'
+      }
+    });
+    const data = 'Resource Deleted...';
+    return data;
+  }
+}
