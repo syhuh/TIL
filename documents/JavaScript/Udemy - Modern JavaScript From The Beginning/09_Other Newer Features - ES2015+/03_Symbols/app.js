59065f1c54a692d2065b5db5061f4a97f9324be5
syhuh <syhuh1125@gmail.com>
2019-10-29 09:59:34 +0900

    Add 09_Other Newer Features - ES2015+/03_Symbols

---
+++ documents/JavaScript/Udemy - Modern JavaScript From The Beginning/09_Other Newer Features - ES2015+/03_Symbols/app.js
@@ -0,0 +1,30 @@
+// Create a symbol
+// const sym1 = Symbol();
+// const sym2 = Symbol('sym2');
+
+// console.log(typeof sym2);
+
+// console.log(Symbol() === Symbol());
+// console.log(`Hello ${sym2.toString()}`);
+
+//Unique Object Keys
+const KEY1 = Symbol();
+const KEY2 = Symbol('sym2');
+
+const myObj = {};
+
+myObj[KEY1] = 'Prop1';
+myObj[KEY2] = 'Prop2';
+myObj.key3 = 'Prop3';
+myObj.key4 = 'Prop4';
+
+// console.log(myObj);
+
+// Symbols are not enumerable in for...in
+// for(let i in myObj) {
+//   console.log(`${i}: ${myObj[i]}`);
+// }
+
+// Symbols are ignored by JSON.stringify
+console.log(JSON.stringify({key: 'prop'}))
+console.log(JSON.stringify(myObj))
