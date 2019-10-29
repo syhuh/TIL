2c7d1f1acbbb7b13d2ebf4a1d1774780dfd36b4b
syhuh <syhuh1125@gmail.com>
2019-10-29 11:06:02 +0900

    Add 09_Other Newer Features - ES2015+/06_ES6_Sets

---
+++ documents/JavaScript/Udemy - Modern JavaScript From The Beginning/09_Other Newer Features - ES2015+/06_ES6_Sets/app.js
@@ -0,0 +1,54 @@
+// SETS - Store unique values of any type
+
+const set1 = new Set();
+
+// Add values to set
+set1.add(100);
+set1.add('A string');
+set1.add({name: 'John'});
+set1.add(true);
+set1.add(100);
+console.log(set1);
+
+const set2 = new Set([1, {name: 'John'}, 1, true, 'string']);
+console.log(set2);
+
+// Get count
+console.log(set1.size);
+
+// Check for values
+console.log(set1.has(50 + 50));
+console.log(set1.has({name: 'John'}));
+console.log({name: 'John'} === {name: 'John'});
+
+// Delete from set
+set1.delete(100);
+console.log(set1);
+
+// ITERATING THROUGH SETS
+
+// For...of
+for(let item of set1) {
+  console.log(item);
+}
+
+for(let item of set1.keys()) {
+  console.log(item);
+}
+
+for(let item of set1.values()) {
+  console.log(item);
+}
+
+for(let item of set1.entries()) {
+  console.log(item);
+}
+
+// ForEach
+set1.forEach((value) => {
+  console.log(value);
+})
+
+// CONVERT SET TO ARRAY
+const setArr = Array.from(set1);
+console.log(setArr);
