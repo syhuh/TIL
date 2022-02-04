c9d326d7a13202b6f06a13911548883aa2011822
syhuh <syhuh1125@gmail.com>
2022-02-04 09:36:43 +0900

    Add Factorial implementation with Recursion

---
+++ documents/CodingInterview/Udemy - Master Coding Interview DSA/recursion_factorial.js
@@ -0,0 +1,27 @@
+// Write two functions that finds a factorial of any number. 
+// One should use recursive and the other should just use a for loop.
+function findFactorialRecursive(num){
+  let answer;
+
+
+
+  if(num === 1){
+    return 1;
+  }    
+
+  answer = num * findFactorialRecursive(num - 1)
+  return answer;
+}
+
+function findFactorialIterative(num){
+  let answer = 1;
+
+  for(let i = num; i > 0; i--){
+    answer = answer * i
+  }
+
+  return answer;
+}
+
+console.log(findFactorialRecursive(3));
+console.log(findFactorialIterative(3));
